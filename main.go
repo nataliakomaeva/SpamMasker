358343adbab660831a904adc2f0795cef8c3fb77
nataliakomaeva <komaevanprok@mail.ru>
2025-04-18 10:23:43 +0300

    Initial commit

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-func main() {
-	a := []int{10, 20, 30, 40, 50}
-	index := 2
-	a[index] = a[len(a)] - 1]
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
+func main() {
+	fmt.Println("Введите свою строку:")
+	a := bufio.NewScanner(os.Stdin)
+	a.Scan()
+	fmt.Println(Spam(a.Text()))
+}
+
+func Spam(s string) string {
+	b := []byte(s)
+
+	for i := 0; i < len(b)-6; i++ {
+		if string(b[i:i+7]) == "http://" { // маскируемм ссылку после http....
+			for j := i + 7; j < len(b); j++ {
+				if b[j] == ' ' {
+					i = j // перескакиваем, чтобы не проверять символы внутри ссылки
+					break
+				}
+				b[j] = '*'
+			}
+		}
+	}
+	return string(b)
 }
